c711891618d6e4480745eb5e738d2dc71f0ae51f
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-05 01:20:59 +0100

    fix: fix errors with mongoDB connection

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// MongoDB connection
+// MongoDB Connection
+const DB_URI = process.env.MONGO_URI
 mongoose
-  .connect('mongodb://localhost:27017/mydatabase', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
+  .connect(DB_URI)
+  .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
